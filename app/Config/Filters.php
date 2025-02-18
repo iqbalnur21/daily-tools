54174f2a60a23df59fc7502006a6c80cd715c517
m. iqbal nur <m.iqbalnur21.min@gmail.com>
2025-02-18 16:55:48 +0700

    fix filter

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -71,6 +71,8 @@ class Filters extends BaseConfig
             'before' =>
             [
                 '/',
+                'Home',
+                'Home/*',
             ]
         ],
     ];
