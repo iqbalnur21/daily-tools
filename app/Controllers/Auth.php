d0ac1df1d6b030d190b1e0532379868c911114af
m. iqbal nur <m.iqbalnur21.min@gmail.com>
2025-02-18 16:08:53 +0700

    auth module

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\User;
+
+class Auth extends BaseController
+{
+    private $users;
+    public function __construct()
+    {
+        $this->request = \Config\Services::request();
+        $this->users = new User();
+    }
+    public function index()
+    {
+        if (session('user_id')) {
+            return redirect()->to(site_url('Home'));
+        }
+        return view('login');
+    }
+    public function loginProcess()
+    {
+        $post = $this->request->getPost();
+        $query = $this->users->getWhere(['username' => $post['username']]);
+        $user = $query->getRow();
+        if ($user) {
+            if (password_verify($post['password'], $user->password)) {
+                $params = ['user_id' => $user->user_id, 'username' => $user->username];
+                session()->set($params);
+                return redirect()->to(site_url('Home'));
+            } else {
+                return redirect()->back()->with('error', 'Username atau Password Salah');
+            }
+        } else {
+            return redirect()->back()->with('error', 'Username atau Password Salah');
+        }
+    }
+    public function logout()
+    {
+        session()->remove('user_id');
+        session()->set('locale', 'en');
+        return redirect()->to(site_url('/'));
+    }
+}
